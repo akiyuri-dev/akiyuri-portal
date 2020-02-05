c4c5b3848bffaeab1c1f071d01bef718f1186290
唐澤　榛菜　 <haruna_karasawa@hakarasawa.local>
2020-02-05 15:25:18 +0900

    内容追加

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,15 @@
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var headerHight = 100;
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - headerHight;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+});
+
 function meAlert() {
     alert("Hello! I'm Akiyuri!");
 }
