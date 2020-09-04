9b0f618a9a8dff299e8b6fa2e21ea47822af038e
Islandtv Uploadbot <islandtv.jr.send.only@gmail.com>
2020-09-04 21:46:14 +0900

    YouTubeNameShare fix

--- public/js/YouTubeNameShare.js
+++ public/js/YouTubeNameShare.js
@@ -39,7 +39,7 @@ function makeURL(){
 
             var tweetURL = title + "\n" + URL;
 
-            document.getElementById("result-erea").innerHTML += "<p>" + tweetURL + "</p>"
+            document.getElementById("result-erea").innerHTML = "<p>" + tweetURL + "</p>"
                 + '<p uk-margin>'
                 + "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>"
                 + '<a class="twitter-share-button" href="https://twitter.com/share?url='
